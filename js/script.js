5f8347649a317ef38f2e3abf339165c37a458efc
Abdel Souhali <2107301@talnet.nl>
2021-12-09 14:09:58 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function home(){
+   location.replace ("index.html")
+}
+function projecten(){
+    location.replace ("projecten.html")
+    }
+ function overmij(){
+        location.replace ("overmij.html")
+        }
+ function modulen(){
+            location.replace ("module.html")
+            }
